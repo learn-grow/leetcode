5364c0beb22b048c4890a3bfce8f6c8815663991
lishaoyong <lishaoyong521@163.com>
2019-11-25 15:40:07 +0800

    [update] update class name

--- learn-leetcode/src/main/java/com/learn/leetcode/l20191125/TwoSumSolution.java
+++ learn-leetcode/src/main/java/com/learn/leetcode/l20191125/TwoSumSolution.java
@@ -5,7 +5,7 @@ package com.learn.leetcode.l20191125;
  * @version $Id: Solution, v 0.1 2019年11月25日 下午3:24 lisy Exp $
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
  */
-public class Solution
+public class TwoSumSolution
 {
 
     public int[] twoSum(int[] nums, int target)
--- learn-leetcode/src/test/java/com/learn/leetcode/test/l20191125/TwoSumSolutionTest.java
+++ learn-leetcode/src/test/java/com/learn/leetcode/test/l20191125/TwoSumSolutionTest.java
@@ -1,13 +1,13 @@
 package com.learn.leetcode.test.l20191125;
 
-import com.learn.leetcode.l20191125.Solution;
+import com.learn.leetcode.l20191125.TwoSumSolution;
 import org.junit.Test;
 
 /**
  * @author : lisy
  * @version $Id: SolutionTest, v 0.1 2019年11月25日 下午3:31 lisy Exp $
  */
-public class SolutionTest
+public class TwoSumSolutionTest
 {
 
     @Test
@@ -18,8 +18,8 @@ public class SolutionTest
 
         int target = 11;
 
-        Solution solution = new Solution();
-        int[] ints = solution.twoSum(nums, target);
+        TwoSumSolution twoSumSolution = new TwoSumSolution();
+        int[] ints = twoSumSolution.twoSum(nums, target);
         if (ints != null){
             for (int anInt : ints)
             {
