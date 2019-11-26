3f06ac8f18a80431c7648667781bcdeb7229951a
lishaoyong <lishaoyong521@163.com>
2019-11-26 18:49:14 +0800

    [add] 判断是否回文数

---
+++ learn-leetcode/src/main/java/com/learn/leetcode/l20191126_2/IsPalindromeSolution.java
@@ -0,0 +1,47 @@
+package com.learn.leetcode.l20191126_2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author : lisy
+ * @version $Id: IsPalindromeSolution, v 0.1 2019年11月26日 下午6:22 lisy Exp $
+ */
+public class IsPalindromeSolution {
+
+
+    /**
+     * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+     * @param x
+     * @return
+     */
+    public boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+        Set<Character> characters = new HashSet<>();
+
+        int n = s.length();
+        int i = 0, j = n - 1;
+        while (i < n && j >= 0){
+            if (i == j){
+                return true;
+            }
+
+            if (!characters.contains(s.charAt(i))){
+                characters.add(s.charAt(i++));
+            }
+            if (characters.contains(s.charAt(j))){
+                characters.remove(s.charAt(j--));
+            }else {
+                return false;
+            }
+        }
+
+
+        return true;
+    }
+
+
+    public static void main(String args[]){
+        System.out.println(5/2);
+    }
+}
---
+++ learn-leetcode/src/test/java/com/learn/leetcode/test/l20191126_2/IsPalindromeSolutionTest.java
@@ -0,0 +1,20 @@
+package com.learn.leetcode.test.l20191126_2;
+
+import com.learn.leetcode.l20191126_2.IsPalindromeSolution;
+import org.junit.Test;
+
+/**
+ * @author : lisy
+ * @version $Id: IsPalindromeSolutionTest, v 0.1 2019年11月26日 下午6:33 lisy Exp $
+ */
+public class IsPalindromeSolutionTest {
+
+    @Test
+    public void isPalindrome(){
+        IsPalindromeSolution isPalindromeSolution = new IsPalindromeSolution();
+        ;
+
+        System.out.println(isPalindromeSolution.isPalindrome(121));
+
+    }
+}
